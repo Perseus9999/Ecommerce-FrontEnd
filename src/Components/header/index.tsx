08e7ed6f60d83ebaa61bc8e7fa31f36cab4c863c
haqueaminul10 <haqueaminul10100@gmail.com>
2024-11-24 16:35:54 +0600

    project-setup

---
+++ src/Components/header/index.tsx
@@ -0,0 +1,11 @@
+const TopHeader = () => {
+  return (
+    <div className='bg-[#191919] text-[#FFFFFF] '>
+      <div>logo</div>
+      <div>searchBar</div>
+      <div>otheers</div>
+    </div>
+  );
+};
+
+export default TopHeader;
---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import { Fragment } from 'react';
+import TopHeader from '@/Components/header';
+
+export default function Home() {
+  return (
+    <Fragment>
+      <TopHeader />
+    </Fragment>
+  );
+}
