337dd88f46992f85f7671ec076666bbf21f04253
haqueaminul10 <haqueaminul10100@gmail.com>
2024-11-26 01:44:05 +0600

    footer section done

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,46 @@
+const Footer = () => {
+  return (
+    <div className='bg-[#e8e8e8] py-6'>
+      <div className='mx-[10%] grid grid-cols-3 gap-6'>
+        <section>
+          <h2 className='text-[18px] font-bold'>Customer Service</h2>
+          <ul className='mt-2'>
+            <li className='footerlist'>Help Center</li>
+            <li className='footerlist'>Transaction Service Agreement</li>
+            <li className='footerlist'>Foreign Customers</li>
+            <li className='footerlist'>Take our feedback survey</li>
+          </ul>
+        </section>
+
+        <section>
+          <h2 className='text-[18px] font-bold'>Stay connected</h2>
+          <ul className='mt-2'>iconss</ul>
+        </section>
+
+        <section>
+          <h2 className='text-[18px] font-bold'>Collaboration with us</h2>
+          <ul className='mt-2'>
+            <li className='footerlist'>Partnerships</li>
+            <li className='footerlist'>Seller Log In</li>
+          </ul>
+        </section>
+
+        <section className='col-span-2'>
+          <h2 className='text-[18px] font-bold'>Help</h2>
+          <p className='footerlist mt-2'>
+            Help Center, Disputes & Reports, Buyer Protection, Report IPR
+            infringement, Regulated Information, Integrity Compliance,
+            Transparency Center, Submit report (non-registered users)
+          </p>
+        </section>
+
+        <section>
+          <h2 className='text-[18px] font-bold'>EcoHub Groups</h2>
+          <p className='mt-2 footerlist'>EcoHuub Group Website, EcoHuub.</p>
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,14 @@
 import { Fragment } from 'react';
 import TopHeader from '@/components/header';
 import Hero from '@/components/hero';
+import Footer from '@/components/footer';
 
 export default function Home() {
   return (
     <Fragment>
       <TopHeader />
       <Hero />
+      <Footer />
     </Fragment>
   );
 }
