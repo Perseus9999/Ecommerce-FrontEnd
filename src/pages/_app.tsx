3c893e1d3836847ac68e2ce1756b967377612986
haqueaminul10 <haqueaminul10100@gmail.com>
2025-02-09 01:30:10 +0600

    change design

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,5 @@
 import type { AppProps } from 'next/app';
-import Layout from '@/components/layout';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Layout />
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
--- src/types/type.tsx
+++
@@ -1,5 +0,0 @@
-export interface NavItem {
-  id: number;
-  label: string;
-  route: string;
-}
